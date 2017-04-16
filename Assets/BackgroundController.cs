7b1c7bb4b19e1c2b6bfb8de061231edacae45a17
Tolga Şen <tolgasen9558@gmail.com>
2017-04-16 15:56:03 +0300

    Fixed Background Scrolling Problem (Little gap was occuring between two backgrounds)

--- Assets/BackgroundController.cs
+++ Assets/BackgroundController.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour {
 
-	public float scrollSpeed = 2.0f;
-
+	private float scrollSpeed = 10.0f;
 	private float backgroundWidth;
 	private float cameraWidth;
+	private Rigidbody2D rb2d;
 
 	void Start(){
+		rb2d = GetComponent<Rigidbody2D>();
+		rb2d.velocity = new Vector2(-scrollSpeed, 0);
 		backgroundWidth = GetComponentInChildren<SpriteRenderer>().sprite.bounds.size.x;
 		cameraWidth = Camera.main.orthographicSize * 2f * Camera.main.aspect;
+		print(backgroundWidth.ToString());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(transform.position.x + backgroundWidth / 2f <= -cameraWidth / 2f){
+		if(transform.position.x < -backgroundWidth){
 			Reposition();
-			return;
 		}
-		transform.Translate(new Vector3(-scrollSpeed * Time.deltaTime, 0, 0));
 	}
 
 	void Reposition(){
-		transform.Translate(new Vector3(backgroundWidth*2, 0, 0));
+		print("background.transform.x = " + transform.position.x.ToString() + " BEFORE");
+		transform.position += new Vector3(backgroundWidth * 2f, 0, 0);
+		print("background.transform.x = " + transform.position.x.ToString() + " AFTER");
 	}
 }
