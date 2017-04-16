39bd63b8ee8aac00de37dc7f5cba344cd6bcf4ef
Tolga Şen <tolgasen9558@gmail.com>
2017-04-16 15:08:32 +0300

    Initial Commit

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+	public float scrollSpeed = 2.0f;
+
+	private float backgroundWidth;
+	private float cameraWidth;
+
+	void Start(){
+		backgroundWidth = GetComponentInChildren<SpriteRenderer>().sprite.bounds.size.x;
+		cameraWidth = Camera.main.orthographicSize * 2f * Camera.main.aspect;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x + backgroundWidth / 2f <= -cameraWidth / 2f){
+			Reposition();
+			return;
+		}
+		transform.Translate(new Vector3(-scrollSpeed * Time.deltaTime, 0, 0));
+	}
+
+	void Reposition(){
+		transform.Translate(new Vector3(backgroundWidth*2, 0, 0));
+	}
+}
---
+++ Assets/MyBird.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBird : MonoBehaviour {
+
+	public float jumpSpeed = 4.0f;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Jump();
+		}
+	}
+
+	private void Jump(){
+		rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+	}
+}
